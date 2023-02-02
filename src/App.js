bcfe10c44f2a2f5e293bac7680108d6532ee50a9
Flavio Garrido <ing.flaviogarrido@gmail.com>
2023-02-02 14:56:19 -0300

    changtes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import { Lightning } from '@lightningjs/sdk'
 import { InputField } from '@lightningjs/ui'
-import { Keyboard, KEYBOARD_FORMATS } from '@lightningjs/ui-components'
+import { Base, Keyboard, KEYBOARD_FORMATS } from '@lightningjs/ui-components'
 
-export default class App extends Lightning.Component {
+// https://rdkcentral.github.io/Lightning-UI-Components/?path=/docs/elements-keyboard--basic
+
+export default class App extends Base {
   static _template() {
     return {
+      rect: true,
+      color: 0xff142a7c,
+      w: 1920,
+      h: 1080,
+      InputField: {
+        type: InputField,
+        cursor: {
+          rect: true,
+          y: 450,
+          x: 500,
+          w: 400,
+          h: 50,
+          color: 0xffffffff,
+        },
+      },
       MyKeyboard: {
         type: Keyboard,
         formats: KEYBOARD_FORMATS.qwerty,
-      },
-      InputField: {
-        type: InputField,
+        y: 500,
+        x: 500,
       },
     }
   }
